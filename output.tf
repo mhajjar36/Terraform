e75ae5b820752f49d46801a9283049a3697b983a
mhajjar36 <107702633+mhajjar36@users.noreply.github.com>
2022-08-24 12:48:51 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,22 +1,22 @@
 # output.tf
 output "iam_user_name" {
-value = aws_iam_user.miketest.name
+value = aws_iam_user.testuser.name
 }
 
 output "iam_user_id" {
-value = aws_iam_user.miketest.id
+value = aws_iam_user.testuser.id
 }
 
 output "iam_access_key_id" {
-value = aws_iam_access_key.mikeaccess.id
+value = aws_iam_access_key.testuser_access.id
 sensitive = true
 }
 
 output "iam_access_key_secret" {
-value = aws_iam_access_key.mikeaccess.secret
+value = aws_iam_access_key.testuser_access.secret
 sensitive = true
 }
 
 output "iam_policy" {
- value = aws_iam_user_policy.mike_policy.policy
+ value = aws_iam_user_policy.testuser_policy.policy
 }
